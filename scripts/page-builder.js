181a87a9466b109bf36e9ec7caffe04237f30bca
Cole <colemag383@gmail.com>
2018-12-30 00:48:54 +1300

    Refactor to be (mostly) consistent with AirBnB ESLint Guidelines

--- scripts/page-builder.js
+++ scripts/page-builder.js
@@ -1,92 +1,86 @@
-const hotsapi = require("../scripts/hotsapi");
+require('../scripts/hotsapi');
 
-window.onload = async function () {
-    await getHeroList();
-
-    fillHeroSelect();
+function changeHeroHeader(hero) {
+  document.getElementById('hero-name-header').innerText = hero.name;
+  document.getElementById('hero-image-header').src = `${profileImgURL + hero.shortName}.png`;
+}
+function changeTalentDisplay(e) {
+  const path = e.composedPath();
+  Object(path).keys.forEach((i) => {
+    if (i.matches('.hero-level')) {
+      path[i].classList.toggle('talent-display-none');
+      e.stopPropagation();
+    }
+  });
 }
 
-async function fillHeroSelect() {
-    let heroSelect = document.querySelector("#hero-select");
-    let randomOption = document.createElement("option");
+function changeHeroTalents(hero) {
+  // remove any previously existing talents
+  const talentMain = document.querySelector('#hero-talent-main');
+  const matches = talentMain.querySelectorAll('.hero-level');
+  matches.forEach(i => i.remove());
 
-    let heroNames = heroList.map(i => { return [i.short_name, i.name] });
+  // add new talents
+  const [randTalents, levelTalents] = getRandTalents(hero);
 
-    heroNames.sort();
-    for (i in heroNames) {
-        let heroOption = document.createElement("option");
-        heroOption.value = heroNames[i][0];
-        heroOption.text = heroNames[i][1];
-        heroSelect.options.add(heroOption);
-    }
-    randomOption.text = "Random";
-    heroSelect.options.add(randomOption, 0);
-    heroSelect.onchange = heroSelectChange;
-    heroSelect.selectedIndex = -1;
-    document.querySelector("label[for=hero-select]").innerText = "Select a hero: ";
+  Object.keys(levelTalents).forEach((i) => {
+    // create elements
+    const levelTitle = document.createElement('h3');
+    const level = document.createElement('div');
+    const levelTalentDiv = document.createElement('div');
+
+    levelTalentDiv.classList.add('hero-level-talents');
+    level.onclick = changeTalentDisplay;
+    level.className = 'hero-level';
+    levelTitle.innerText = i;
+    talentMain.appendChild(level);
+    level.appendChild(levelTitle);
+    levelTitle.after(levelTalentDiv);
+
+    Object.keys(levelTalents[i]).forEach((j) => {
+      const talentImg = document.createElement('img');
+      const talent = document.createElement('div');
+
+      talent.innerText = levelTalents[i][j].name;
+      talent.className = levelTalents[i][j].talentTreeId === randTalents[i] ? 'talent-chosen' : 'talent-not-chosen';
+      talent.title = levelTalents[i][j].talentTreeId;
+      talentImg.src = talentImgURL + levelTalents[i][j].icon;
+      talentImg.className = levelTalents[i][j].talentTreeId === randTalents[i] ? 'talent-chosen' : 'talent-not-chosen';
+
+      levelTalentDiv.appendChild(talent);
+      talent.appendChild(talentImg);
+    });
+  });
 }
 
 async function heroSelectChange() {
-    let heroSelect = document.getElementById("hero-select");
-    var hero = await getHero(heroSelect.value);
+  const heroSelect = document.getElementById('hero-select');
+  const hero = await getHero(heroSelect.value);
 
-    changeHeroHeader(hero);
-    changeHeroTalents(hero);
+  changeHeroHeader(hero);
+  changeHeroTalents(hero);
 }
 
-function changeHeroHeader(hero) {
-    document.getElementById("hero-name-header").innerText = hero.name;
-    document.getElementById("hero-image-header").src = profileImgURL + hero.shortName + ".png";
+async function fillHeroSelect() {
+  const heroSelect = document.querySelector('#hero-select');
+  const randomOption = document.createElement('option');
+
+  const heroNames = heroList.map(i => [i.short_name, i.name]);
+
+  heroNames.sort();
+  heroNames.forEach((i) => {
+    const heroOption = document.createElement('option');
+    [heroOption.value, heroOption.text] = i;
+    heroSelect.options.add(heroOption);
+  });
+  randomOption.text = 'Random';
+  heroSelect.options.add(randomOption, 0);
+  heroSelect.onchange = heroSelectChange;
+  heroSelect.selectedIndex = -1;
+  document.querySelector('label[for=hero-select]').innerText = 'Select a hero: ';
 }
 
-
-
-function changeHeroTalents(hero) {
-    //remove any previously existing talents
-    let talentMain = document.querySelector('#hero-talent-main');
-    let matches = talentMain.querySelectorAll('.hero-level');
-    matches.forEach(i => i.remove());
-
-    //add new talents
-    let tempTalents = getRandTalents(hero);
-    let randTalents = tempTalents[0];
-    let levelTalents = tempTalents[1];
-
-    for (let i in levelTalents) {
-        //create elements
-        let levelTitle = document.createElement('h3');
-        let level = document.createElement('div');
-        let levelTalentDiv = document.createElement('div');
-
-        levelTalentDiv.classList.add("hero-level-talents");
-        level.onclick = changeTalentDisplay;
-        level.className = "hero-level"
-        levelTitle.innerText = i;
-        talentMain.appendChild(level);
-        level.appendChild(levelTitle);
-        levelTitle.after(levelTalentDiv);
-
-        for (let j in levelTalents[i]) {
-            let talentImg = document.createElement('img');
-            let talent = document.createElement('div');
-
-            talent.innerText = levelTalents[i][j].name;
-            talent.className = levelTalents[i][j].sort === randTalents[i] ? "talent-chosen" : "talent-not-chosen";
-            talentImg.src = talentImgURL + levelTalents[i][j].icon
-            talentImg.className = levelTalents[i][j].sort === randTalents[i] ? "talent-chosen" : "talent-not-chosen";
-
-            levelTalentDiv.appendChild(talent);
-            talent.appendChild(talentImg);
-        }
-    }
-}
-function changeTalentDisplay(e) {
-    let path = e.composedPath();
-    for (i in path) {
-        if (path[i].matches('.hero-level')) {
-            path[i].classList.toggle('talent-display-none');
-            e.stopPropagation();
-            return;
-        }
-    }
-}
+window.onload = async () => {
+  await getHeroList();
+  fillHeroSelect();
+};
